7eafc864e720a1d73c31388102fc4acef3f2b361
pettamrakesh3 <122041480+pettamrakesh3@users.noreply.github.com>
2023-07-12 01:59:22 +0530

    Add files via upload

---
+++ socket/index.js
@@ -0,0 +1,51 @@
+const io=require("socket.io")(8900, {
+    cors:{
+        origin: "http://localhost:3000",
+    },
+});
+
+let users=[];
+
+const addUser = (userId,socketId) =>{
+    !users.some((user)=>user.userId===userId) &&
+    users.push({userId,socketId});
+}
+
+const removeUser = (socketId)=>{
+    users =users.filter((user)=>user.socketId!==socketId);
+}
+
+const getUser =(userId)=>{
+    return users.find((user)=>user.userId===userId);
+}
+
+io.on("connection", (socket)=>{
+    //when connects
+    console.log("a user connected");
+
+    //take userId and socket Id from user
+
+    socket.on("adduser",(userId)=>{
+    addUser(userId,socket.id);
+    io.emit("getUser",users);
+    })
+
+    
+    //send and get a message
+
+    socket.on("sendMessage",({senderId,receiverId,text})=>{
+        const user=getUser(receiverId);
+        io.to(user.socketId).emit("getMessage",{
+            senderId,
+            text,
+        })
+    })
+
+    //when disconnects
+
+    socket.on("disconnect", ()=>{
+        console.log("a user disconnected");
+        removeUser(socket.id);
+        io.emit("getUsers",users);
+    })
+})
